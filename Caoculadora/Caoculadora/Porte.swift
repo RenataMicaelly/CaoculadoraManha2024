986423db8096c7733c339402e1f5fa0bd3ba1c5a
Renata Micaelly <renatamicaelly@Renatas-MacBook-Pro.local>
2024-06-17 11:37:14 -0300

    fix variables

--- Caoculadora/Caoculadora/Porte.swift
+++ Caoculadora/Caoculadora/Porte.swift
@@ -8,20 +8,20 @@
 import Foundation
 
 enum Porte: String, CaseIterable {
-    case pequeno = "Pequeno"
-    case medio = "Médio"
-    case grande = "Grande"
+    case small = "Pequeno"
+    case medium = "Médio"
+    case big = "Grande"
 
     func calcularIdade(deAnos anosCaninos: Int,
                       eMeses mesesCaninos: Int) -> Int {
         let multiplicador: Int
         
         switch self {
-        case .pequeno:
+        case .small:
             multiplicador = 6
-        case .medio:
+        case .medium :
             multiplicador = 7
-        case .grande:
+        case .big :
             multiplicador = 8
         }
 
